56127e8864f6495a3195a23033755d782f272002
ken2063 <ken2063@naver.com>
2024-04-08 10:17:36 +0900

    😤 뷰 커밋 완료

--- app/vite.config.js
+++ app/vite.config.js
@@ -14,6 +14,7 @@ import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
   plugins: [vue()],
+  // base: "/vue-setting/",
   resolve: {
     alias: { "@": fileURLToPath(new URL("./src", import.meta.url)) },
   },
