74d16973b08f89ceac76009d951d956773076749
ken2063 <ken2063@naver.com>
2024-04-09 11:59:29 +0900

    😤 뷰 커밋 완료

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,5 +1,5 @@
 // src/router/index.js
-import { createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHashHistory} from 'vue-router'
 const routes = [
     {
         path: '/',
@@ -35,8 +35,8 @@ const routes = [
     // not found
 ]
 const router = createRouter({
-    // history: createWebHistory(),
-    history: createWebHistory(process.env.BASE_URL),
+    history: createWebHashHistory(),
+    // history: createWebHistory(process.env.BASE_URL),
     routes,
 })
 export default router
