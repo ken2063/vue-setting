362c18dd62f6cc9b4ed2abffa3ff65cf6b273749
ken2063 <ken2063@naver.com>
2024-04-09 17:04:16 +0900

    😤 뷰 커밋 완료

--- app/vite.config.js
+++ app/vite.config.js
@@ -22,6 +22,6 @@ export default defineConfig({
     port: 3000
   },
   build: {
-    outDir: '../docs/'
+    outDir: '/docs/'
   }
 });
--- docs/assets/notFound-DmWY6lVE.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,o as t,a as s}from"./index-DlWslZVY.js";const n={},c=s("h1",null,"페이지를 찾을 수 없습니다.",-1),a=[c];function r(_,d,l,p,i,u){return t(),o("div",null,a)}const h=e(n,[["render",r]]);export{h as default};
