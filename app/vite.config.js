efe48cc213a4054d314683de97b050938f73155a
ken2063 <ken2063@naver.com>
2024-04-11 09:43:08 +0900

    😤 뷰 커밋 완료

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,20 +1,9 @@
-// vite 초기셋팅
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [vue()],
-// })
-// vite 초기셋팅
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
   plugins: [vue()],
-  // base: "/vue-setting/",
   resolve: {
     alias: { "@": fileURLToPath(new URL("./src", import.meta.url)) },
   },
