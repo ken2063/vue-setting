8b6a8b79001993dc329347dc85380e0aeb8dc316
ken2063 <ken2063@naver.com>
2024-04-15 17:16:57 +0900

    😤 뷰 커밋 완료

--- app/src/layout/DefaultFooter.vue
+++ app/src/layout/DefaultFooter.vue
@@ -8,9 +8,32 @@ export default {
 
 <template>
   <footer>
+    <!-- foot -->
     <div class="foot">
-      푸터
+      <!-- footmenu -->
+      <div class="footmenu flex">
+        <div class="menuitem">회사소개</div>
+        <div class="menuitem">인재채용</div>
+        <div class="menuitem">이용약관</div>
+        <div class="menuitem">개인정보처리방침</div>
+      </div>
+      <!-- // footmenu -->
+
+      <!-- footwrap -->
+      <div class="footwrap flex">
+        <div class="footlogo">LOGO</div>
+
+        <div class="foottxt">
+          <address>
+            경기도 안산시 선부동 선부광장 서로 42 106동 1204호
+          </address>
+
+          <span>LOGO Co., Ltd.</span>
+        </div>
+      </div>
+      <!-- // footwrap -->
     </div>
+    <!-- // foot -->
   </footer>
 </template>
 
